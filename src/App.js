4c78cc5d7bf5f9fd6c404ca63d2c2841099e804b
Duncan <duncanwoodpro@gmail.com>
2023-02-25 12:48:02 -0500

    setup all my files so that the routes will work as a single-page application. now onto mapping my array of breeds.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import './App.css';
-import { Routes, Route } from "react-router-dom"
-import { useState, useEffect } from "react"
-import  axios  from 'axios'
-import { DataContext } from './DataContext'
-
-import Home from './components/Home'
+import "./App.css";
+import Header from './components/Header'
+import Main from "./components/Main";
 
 function App() {
-  const [dogs, setDogs] = useState([])
-  const [selectedDog, setSelectedDog] = useState(null)
-
-  useEffect(() => {
-    const getDogs = async () => {
-      const response = await axios.get(`https://api.thedogapi.com/v1/images/search`)
-      console.log(response.data)
-      console.log(response.data.url)
-      setDogs(response.data)
-    }
-
-    getDogs()
-  }, [])
-
   return (
     <div className="App">
-      <DataContext.Provider value={{dogs, setDogs}}>
-        <Home />
-      </DataContext.Provider>    
+      <div className="header-container">
+        <Header />
       </div>
+      <div className="main-container">
+        <Main />
+      </div>
+    </div>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,23 @@
-import React, { useContext } from 'react';
-import { DataContext } from '../DataContext';
+import { useState, useEffect } from "react"
+import  axios  from 'axios'
+
 
 export default function Home () {
-    const { dogs } = useContext(DataContext);
+    const [randomDogImage, setRandomDogImage] = useState([])
 
+    useEffect(() => {
+        const getRandomDogImage = async () => {
+          const response = await axios.get(`https://api.thedogapi.com/v1/images/search`)
+          console.log(response.data)
+          setRandomDogImage(response.data[0])
+        }
+    
+        getRandomDogImage()
+      }, [])
     return (
         <div>
             <h1>Welcome to Dog City!</h1>
-            {/* This code is a placeholder and will probably need to change. in the future, I will add a variable for index to use for the default random dog on the starting page. */}
-            {dogs.length > 0 && (
-            <img src={dogs[0].url} alt="random dog photo" style={{width:'50%'}}/>
-            )}
+            {randomDogImage ? <img src={randomDogImage.url} alt="random dog photo" style={{width:'50%'}}/> : "dog loading"}
         </div>
     )
 }
